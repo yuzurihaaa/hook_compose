d28f29e1239271c50bc32296beceaa8f97af30db
yuzuriha <zaralockheart@gmail.com>
2020-01-05 12:04:08 +0800

    update example

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,7 @@ class MyHomePage extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final _formKey = useFormKey();
+    print('rendered');
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
